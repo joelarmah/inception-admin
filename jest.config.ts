b4fc53899b1f3bcf9d12641ddcd16af93fc5d9e7
Joel <hamra64@gmail.com>
2025-10-08 17:38:25 +0000

    chore: Fix babel issue with npm build

--- jest.config.ts
+++ jest.config.ts
@@ -142,7 +142,7 @@ const config: Config = {
   // setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  // setupFilesAfterEnv: [],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
@@ -182,7 +182,7 @@ const config: Config = {
 
   // A map from regular expressions to paths to transformers
   transform: {
-    '^.+\\.(t|j)sx?$': 'babel-jest',
+    "^.+\\.(t|j)sx?$": ["babel-jest", { configFile: "./babel-jest.config.js" }],
   },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
